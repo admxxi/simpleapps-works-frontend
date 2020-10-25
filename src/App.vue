3d3561fb0499ba4ed9ed7eb39205d25cfeb4f00e
Andre Monteiro <admxxi@gmail.com>
2020-10-25 15:53:46 +0000

    [Setup] Small Changes on App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,54 @@
 <template>
-  <div id="app">
-    <HelloWorld msg="Simple Apps" />
-  </div>
+  <v-app>
+    <v-app-bar app color="primary" dark>
+      <div class="d-flex align-center">
+        <v-img
+          alt="SimpleApps Logo"
+          class="shrink mr-2"
+          contain
+          :src="require('./assets/logo.svg')"
+          transition="scale-transition"
+          width="40"
+        />
+
+        <h2>Simple Apps</h2>
+      </div>
+
+      <v-spacer></v-spacer>
+
+      <span>
+        <span class="mr-2">Version 0.1.0 Beta</span>
+        <v-icon>code</v-icon>
+      </span>
+    </v-app-bar>
+    <v-main> </v-main>
+    <BottomNav />
+  </v-app>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
 import HelloWorld from './components/HelloWorld.vue'
+import TopToolbar from '@/components/TopToolbar.vue'
+import BottomNav from '@/components/BottomNav.vue'
 
 export default Vue.extend({
   name: 'App',
+
   components: {
+    BottomNav,
+    TopToolbar,
     HelloWorld
-  }
+  },
+
+  data: () => ({
+    //
+  })
 })
 </script>
 
 <style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  background-color: #7d121f;
 }
 </style>
