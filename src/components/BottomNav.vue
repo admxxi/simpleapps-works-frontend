f99bd857f4eeb5ed8c3c5c02144a5be66664d26e
Andre Monteiro <admxxi@gmail.com>
2020-10-25 15:54:17 +0000

    [Setup] TopBar + BottomNav.vue From Tutorial

---
+++ src/components/BottomNav.vue
@@ -0,0 +1,12 @@
+<template>
+  <v-bottom-navigation app fixed grow v-model="activeItem" color="primary"> </v-bottom-navigation>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+
+@Component
+export default class BottomNav extends Vue {
+  activeItem: string = 'top'
+}
+</script>
